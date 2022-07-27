faf994c6ca2e230476b3ede5a873f71ddd0be933
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-07-27 15:27:26 +0200

    show favs ok

--- js/favorites.js
+++ js/favorites.js
@@ -2,22 +2,21 @@
 document.addEventListener('DOMContentLoaded', function loaded() {
     const api = '69abcdc201c0712b6a89b7fe65700125'
 
-    var article = document.querySelector('.film');
-    var article2 = document.querySelector('.tv');
+    var film = document.querySelector('.film');
+    var show = document.querySelector('.tv');
 
     fetch('./controller/favoris.php?action=sortmov')
         .then(response => response.json())
         .then(data => {
 
             if (data["code"] == 10) {
+
                 if (data.movie.length != 0) {
 
-                    var titre = document.createElement('h1')
-                    titre.textContent = 'Vos films favoris'
+                    const title = document.createElement('h1')
+                    title.textContent = 'Vos films favoris'
+                    film.appendChild(title);
                     
-                    var cin = document.querySelector('.film')
-                    cin.prepend(titre)
-
                     for (var i = 0; i < data.movie.length; i++) {
                         fetch("http://api.themoviedb.org/3/movie/" + data.movie[i] + "?api_key=" + api + "&language=fr-FR")
                             .then(response => response.json())
@@ -26,25 +25,16 @@ document.addEventListener('DOMContentLoaded', function loaded() {
                                 var item = data;
 
                                 let link = document.createElement('a');
-                                link.href = './detail.php?movie=' + item.id;
+                                link.href = './movie.php?type=movie&id=' + item.id;
 
                                 let img = document.createElement('img');
                                 img.src = 'https://image.tmdb.org/t/p/w500/' + item.poster_path;
                                 img.alt = item.title;
 
-                                // let title = document.createElement('p');
-                                // title.innerHTML = item.title;
-
-                                // let ranking = document.createElement('p');
-                                // ranking.innerHTML = 'Note: '+item.vote_average+'/10';
-
-                                article.appendChild(link);
+                                film.appendChild(link);
                                 link.appendChild(img);
-                                // link.appendChild(title);
-                                // link.appendChild(ranking);
 
                             })
-
 
                     }
 
@@ -57,11 +47,9 @@ document.addEventListener('DOMContentLoaded', function loaded() {
 
                 if (data.tv.length != 0) {
 
-                    var titre2 = document.createElement('h1')
-                    titre2.textContent = 'Vos series favorites'
-
-                    var tvshow = document.querySelector('.tv')
-                    tvshow.prepend(titre2)
+                    let title2 = document.createElement('h1')
+                    title2.textContent = 'Vos series favorites'
+                    show.appendChild(title2)
 
                     for (var i = 0; i < data.tv.length; i++) {
 
@@ -72,36 +60,30 @@ document.addEventListener('DOMContentLoaded', function loaded() {
                                 var item = data;
 
                                 let link = document.createElement('a');
-                                link.href = './detail.php?tv=' + item.id;
+                                link.href = './show.php?type=tv&id=' + item.id;
 
                                 let img = document.createElement('img');
                                 img.src = 'https://image.tmdb.org/t/p/w500/' + item.poster_path;
                                 img.alt = item.name;
 
-                                // let title = document.createElement('p');
-                                // title.innerHTML = item.name;
-
-                                // let ranking = document.createElement('p');
-                                // ranking.innerHTML = 'Note: '+item.vote_average+'/10';
-
-                                article2.appendChild(link);
+                                show.appendChild(link);
                                 link.appendChild(img);
-                                // link.appendChild(title);
-                                // link.appendChild(ranking);
 
                             })
                     }
 
                 } else {
+                    
                     var h2 = document.createElement('h2')
                     h2.textContent = "Vous n'avez pas encore ajouté de séries à vos favoris."
-                    serie.appendChild(h2)
+                    show.appendChild(h2)
                 }
+
             } else {
 
                 var h1 = document.createElement('h1');
                 h1.textContent = data['message'];
-                article.appendChild(h1);
+                film.appendChild(h1);
             }
         })
 })
